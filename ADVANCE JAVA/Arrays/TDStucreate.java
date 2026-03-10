afaa7ebe0df8eab54f914b965251152e654fa262
Kamaldas970 <baleraokamal97050sravan@gmail.com>
2026-03-10 11:14:34 +0530

    Refactor TDStucreate to use jagged arrays
    Modified the marks array to use jagged arrays and added pass/fail output based on marks.

--- ADVANCE JAVA/Arrays/TDStucreate.java
+++ ADVANCE JAVA/Arrays/TDStucreate.java
@@ -1,7 +1,12 @@
 import java.util.*;
 public class TDStucreate{
     public static void main(String args[]){
-        int marks[][]=new int[3][4];
+        int marks[][]=new int[3][];
+        
+        marks[0]=new int[4];
+        marks[1]=new int[2];
+        marks[2]=new int[3];
+        
         Scanner sc=new Scanner(System.in);
 
         for(int i=0;i<marks.length;i++){
@@ -11,14 +16,22 @@ public class TDStucreate{
                 marks[i][j]=sc.nextInt();
 
              }
+           
              
         }
         for(int i=0;i<marks.length;i++){
             System.out.println("student class is " +i);
             for(int j=0;j<marks[i].length;j++){
                 System.out.println("student marks.. " + marks[i][j]);
+                  System.out.println();
+               if(marks[i][j]>50){
+                System.out.println("Pass");
+                
+             }else{
+                System.out.println("Fail");
              }
-             System.out.println();
+             }
+           
         }
         
 
