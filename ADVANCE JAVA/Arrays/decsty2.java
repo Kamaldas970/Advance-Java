8999e8ad207a11716450a3119b5c401dcd742094
Kamaldas970 <baleraokamal97050sravan@gmail.com>
2026-03-11 08:26:29 +0530

    Add files via upload

---
+++ ADVANCE JAVA/Arrays/decsty2.java
@@ -0,0 +1,18 @@
+package Arrays;
+public class decsty2{
+    
+    public static void main(String args[]){
+        int arr[]={4,6,8,10,12};
+
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.println("The index no...  "+i+ " The element is" + arr[i]);
+        // }
+        
+        for(int a:arr){
+            System.out.println(" The element is: " + a);
+
+        }
+
+    }
+
+}
---
+++ ADVANCE JAVA/Arrays/multidecsty.java
@@ -0,0 +1,12 @@
+package Arrays;
+public class multidecsty{
+    public static void main(String args[]){
+        int arr[][]={{2,4},{2,4,5}};
+        for(int row[]:arr){
+            for(int a:row){
+                System.out.println(a);
+            }
+            System.out.println();
+        }
+}
+}
